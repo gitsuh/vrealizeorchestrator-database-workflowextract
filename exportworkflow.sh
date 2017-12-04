4483343ec3af9df143c1c4abbcfb857ddb90ef75
good-paste <good-paste@users.noreply.github.com>
2017-12-04 16:29:41 -0500

    Update exportworkflow.sh

--- exportworkflow.sh
+++ exportworkflow.sh
@@ -1,15 +1,13 @@
-#!/bin/bash
+#init
 function extractworkflow(){
-psql -d vmware -c "SELECT xmlcontent FROM vmo_workflowcontent WHERE workflowid = '$*'" > /tmp/recoveryattempt/tempfiles/workflow-content
-sed -i 1d /tmp/recoveryattempt/tempfiles/workflow-content
-sed -i 1d /tmp/recoveryattempt/tempfiles/workflow-content
-sed -i 's/.$//' /tmp/recoveryattempt/tempfiles/workflow-content
+psql -qAtX -d vmware -c "SELECT xmlcontent FROM vmo_workflowcontent WHERE workflowid = '$*'" > /tmp/recoveryattempt/tempfiles/workflow-content.temp
 VARFILENAME=$(cat /tmp/recoveryattempt/tempfiles/workflow-content | grep -o -P '(?<=CDATA\[).*(?=\]\])' |head -n 1 )
 VARFILENAME=${VARFILENAME// /_}
 VARFILENAME=$VARFILENAME$*
-zip /tmp/recoveryattempt/finishedfile/$VARFILENAME.workflow /tmp/recoveryattempt/tempfiles/workflow-content /tmp/recoveryattempt/staticfiles/workflow-info /tmp/recoveryattempt/staticfiles/workflow-versionhistory
+zip -0 -j /tmp/recoveryattempt/finishedfile/$VARFILENAME.workflow /tmp/recoveryattempt/tempfiles/workflow-content /tmp/recoveryattempt/staticfiles/workflow-info /tmp/recoveryattempt/staticfiles/workflow-versionhistory
 echo "Generated file from workflow name $VARFILENAME with workflowID $*"
 }
+
 
 FILE=$1
 while IFS=, read -r workflowid
